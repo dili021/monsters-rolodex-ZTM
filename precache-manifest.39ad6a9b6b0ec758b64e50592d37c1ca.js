88aa71903d1be7e802f8224634287b7385e5bf6f
stefan dili <stefan.dili.021@gmail.com>
2020-08-02 14:46:13 +0200

    Updates

---
+++ precache-manifest.39ad6a9b6b0ec758b64e50592d37c1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c78546f2eef955e748f32517c339fa",
+    "url": "/monsters-rolodex-ZTM/index.html"
+  },
+  {
+    "revision": "9de8814e627f70e75f85",
+    "url": "/monsters-rolodex-ZTM/static/css/main.fd7ebe5f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex-ZTM/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-ZTM/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de8814e627f70e75f85",
+    "url": "/monsters-rolodex-ZTM/static/js/main.74860862.chunk.js"
+  },
+  {
+    "revision": "01918a164c4683a7d268",
+    "url": "/monsters-rolodex-ZTM/static/js/runtime-main.8a71383f.js"
+  }
+]);
